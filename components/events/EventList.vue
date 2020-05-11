1e65e71972c9ec3c2d42401720b7649066cc3c33
Jeremías <jimmyguionbajo@gmail.com>
2020-05-11 14:17:21 -0400

    INIT

---
+++ components/events/EventList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Eventos Lista</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <event-list></event-list>
+</template>
+
+<script>
+import EventList from '~/components/events/EventList.vue'
+
+export default {
+  components: {
+    EventList
+  }
+}
+</script>
