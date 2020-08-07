cddfc761b9081c29f270804268d2fa20ccbf43f7
javierc22 <javier.ec.22@gmail.com>
2020-08-07 17:03:19 -0400

    Agregar diseño Formulario Login

---
+++ components/forms/LoginForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card flat>
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src="require('~/assets/user-login.png')"
+      contain
+    ></v-img>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+          label="Correo electrónico"
+          v-model="userEmail"
+          type="text"
+          outlined
+          dense
+          color="grey darken-2"
+          background-color="grey lighten-4"
+        ></v-text-field>
+
+        <v-text-field
+          label="Contraseña"
+          v-model="userPassword"
+          type="password"
+          outlined
+          dense
+          color="grey darken-2"
+          background-color="grey lighten-4"
+        ></v-text-field>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn 
+        class="py-5 mx-auto"
+        color="info"
+        @click="submitForm"
+      >Ingresar</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    email: String,
+    password: String,
+    submitForm: Function
+  },
+  computed: {
+    userEmail: {
+      get() { return this.email },
+      set(value) { this.$emit('update-email', value) }
+    },
+    userPassword: {
+      get() { return this.password },
+      set(value) { this.$emit('update-password', value) }
+    }
+  },
+}
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,25 +1,56 @@
 <template lang="html">
-  <div>
-    <v-text-field v-model="email" label="Email"></v-text-field>
-    <v-text-field
-      v-model="password"
-      label="Password"
-      type="password"
-    ></v-text-field>
-    <v-btn @click="login()"> Login </v-btn>
-  </div>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-12 mx-auto" max-width="500" height="600">
+          <v-tabs background-color="primary" dark v-model="tab" centered>
+            <v-tab>
+              Login
+            </v-tab>
+            <v-tab>
+              Sign Up
+            </v-tab>
+          </v-tabs>
+          <v-tabs-items v-model="tab">
+            <v-tab-item>
+              <login-form 
+                :email="email" 
+                :password="password" 
+                :submitForm="login"
+                @update-email="updateEmail"
+                @update-password="(value) => {password = value}"
+              />
+            </v-tab-item>
+            <v-tab-item>
+              <v-card flat>
+                <v-card-text>Contenidow</v-card-text>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
+import LoginForm from '~/components/forms/LoginForm';
+
 export default {
   layout: 'session',
+
+  components: {
+    LoginForm
+  },
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      tab: null
     }
   },
   methods: {
     async login() {
+      console.log(this.password)
       try {
         const response = await this.$auth.login({
           data: { email: this.email, password: this.password }
@@ -31,6 +62,9 @@ export default {
       } catch (err) {
         console.log(err)
       }
+    },
+    updateEmail(value) {
+      this.email = value;
     }
   },
 }
